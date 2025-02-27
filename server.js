9e27626f1e5c852a1f73a1f9209f1edec31aa984
matvej668 <121830011+matvej668@users.noreply.github.com>
2025-02-27 23:06:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080; // Использует PORT из окружения или 8080
 
+// Раздача статических файлов из папки public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Обработчик главной страницы
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index2.html"));
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
     console.log(`🚀 Сервер запущен на http://localhost:${PORT}`);
 });
