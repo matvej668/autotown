e81d05655a4836d61c09370796c929b9c8d16c2e
matvej668 <121830011+matvej668@users.noreply.github.com>
2025-02-27 22:48:51 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Сервер запущен на http://localhost:${PORT}`);
+});
